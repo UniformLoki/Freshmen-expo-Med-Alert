f4d8ba21148e03579abac63527d53c97fd426bb1
Elia <elia.browning205@gmail.com>
2024-04-04 13:54:30 -0500

    Added layout.html and extended template to test file

--- gui/gui.py
+++ gui/gui.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('test.html', title='Test Page', header='TEST PAGE')
+    return render_template('test.html', header='TEST PAGE')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
