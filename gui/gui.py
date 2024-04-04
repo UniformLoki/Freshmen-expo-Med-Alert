ba7e5154706cbad2809595816c727ae43e77a85b
Elia <elia.browning205@gmail.com>
2024-04-04 13:34:29 -0500

    Set up Flask file directory

---
+++ gui/gui.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('test.html', title='Test Page', header='TEST PAGE')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
