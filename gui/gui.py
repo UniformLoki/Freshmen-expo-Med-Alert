5ef8526095dbb6886318a06ac681967d1be4abf4
Elia <elia.browning205@gmail.com>
2024-04-04 23:04:51 -0500

    Finished navbar

--- gui/gui.py
+++ gui/gui.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('test.html', header='TEST PAGE')
+def home_page():
+    return render_template('test.html', header='Home page')
+
+@app.route('/alarms')
+def alarm_page():
+    return render_template('test.html', header='Alarm page')
+
+@app.route('/meds')
+def meds_page():
+    return render_template('test.html', header='Meds page')
+
+@app.route('/profiles')
+def profile_page():
+    return render_template('test.html', header='Profile page')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
