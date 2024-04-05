95d16c88d79e7745507519e49f56e3d0ae5ef292
Elia <elia.browning205@gmail.com>
2024-04-04 23:09:36 -0500

    Added comments because i was being Stupid

--- gui/gui.py
+++ gui/gui.py
@@ -18,5 +18,6 @@ def meds_page():
 def profile_page():
     return render_template('test.html', header='Profile page')
 
+# run locally
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
