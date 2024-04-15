90511f36f2cb81f50a40ab6541e5bbfa935f3b93
Rachel <rad044@latech.edu>
2024-04-15 17:53:43 -0500

    can now detect how many pills were removed

--- pill_detection/pill_detection.ino
+++ pill_detection/pill_detection.ino
@@ -3,12 +3,14 @@
 
 #include "HX711.h" 
 
-#define calibration_factor  28510.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
+#define CALIBRATION_FACTOR  28510.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
 
 #define LOADCELL_DOUT_PIN  3
 #define LOADCELL_SCK_PIN  2
 
-float single_pill = 0
+float SinglePill = 0;
+float StartingMass = 0;
+float PRECISION = 0.001; //mass changes will be recognized if they are within +/- this number from the target
 
 HX711 scale;
 
@@ -19,29 +21,55 @@ void setup() {
 
   //set up scale
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  scale.set_scale(calibration_factor);
+  scale.set_scale(CALIBRATION_FACTOR);
   scale.tare();
 
-  serial.println("Place a single pill on the scale, then press enter.");
-  serial.println("this calibrates the scale so it can recognize how many pills were removed.");
+  Serial.println("Place a single pill on the scale, then press enter.");
+  Serial.println("This calibrates the scale so it can recognize how many pills were removed.");
 
   while (Serial.available() == 0){} //pauses code until a key is pressed
+  Serial.parseInt(); //clear serial monitor
 
-  single_pill = scale.get_units();
+  //get mass of single pill
+  SinglePill = scale.get_units();
   Serial.print("One pill weighs ");
-  Serial.print(single_pill, 3);
+  Serial.print(SinglePill, 3);
   Serial.print(" kg"); 
   Serial.println();
 
-  serial.println("Place the bottle of pills on the scale.")
-  serial.println("Press enter to start tracking pills")
+  Serial.println("Place the bottle of pills on the scale.");
+  Serial.println("Press enter to start tracking pills");
 
   while (Serial.available() == 0){} //pauses code until a key is pressed
+  Serial.parseInt(); //clear serial monitor
+
+  //get initial mass of bottle and pills
+  StartingMass = scale.get_units();
+
+  Serial.println("Pill tracking started");
 
 }
 
 void loop() {
-  
+ 
+  //get current mass on scale
+  float CurrentMass = scale.get_units();
+
+  //find difference in mass
+  float MassDifference = StartingMass - CurrentMass;
+
+  //check for pill removal
+  if (MassDifference >= SinglePill - PRECISION){
+
+    int NumPillsTaken = MassDifference / SinglePill;
+    Serial.print(NumPillsTaken);
+    Serial.println("pill(s) were removed");
+    StartingMass = CurrentMass;
+
+  }
+
+  //delay 3s between removal checks
+  delay(3000);
 
 }
 
