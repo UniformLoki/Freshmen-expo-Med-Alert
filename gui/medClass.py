6480fb1bbe27e51c5a1ff2d56782a035342de03b
Elia <elia.browning205@gmail.com>
2024-04-29 18:18:32 -0500

    Deleted redundant accessors and mutators

--- gui/medClass.py
+++ gui/medClass.py
@@ -1,6 +1,4 @@
-import uuid
-
-class Medication():
+class Medication:
 
     def __init__(self, medID:int, profile:int, name:str, dose:str, full_amount:float, current_amount:float, schedule:list[str], pill_weight:float) -> None:
         self.id = medID
@@ -11,32 +9,3 @@ class Medication():
         self.current_amount = current_amount
         self.schedule = schedule
         self.pill_weight = pill_weight
-
-    @property
-    def name(self) -> str:
-        # decryption stuff
-        return self._name
-    
-    @name.setter
-    def name(self, new_name:str) -> None:
-        # encryption stuff
-        self._name = new_name
-
-    @property
-    def dose(self) -> str:
-        # decryption stuff
-        return self._dose
-    
-    @dose.setter
-    def dose(self, new_dose:str) -> None:
-        # encryption stuff
-        self._dose = new_dose
-
-    @property
-    def current_amount(self) -> float:
-        return self._current_amount
-    
-    @current_amount.setter
-    def current_amount(self, new_amount:float) -> None:
-        if 0 <= new_amount <= self.full_amount:
-            self._current_amount = new_amount
--- gui/profileClass.py
+++ gui/profileClass.py
@@ -1,5 +1,3 @@
-import uuid, datetime, re
-
 class Profile:
 
     def __init__(self, profileID:int, added:str, name:str, birthdate:str, email:str=None, phone:str=None) -> None:
@@ -9,37 +7,3 @@ class Profile:
         self.birthdate = birthdate
         self.email = email
         self.phone = phone
-
-    @property
-    def birthdate(self) -> str:
-        return self._birthdate
-    
-    @birthdate.setter
-    def birthdate(self, new_birthdate:str) -> None:
-        format = "%m/%d/%Y"
-        new_birthdate_formatted = datetime.datetime.strptime(new_birthdate, format)
-        current_time = datetime.datetime.now()
-        if new_birthdate_formatted <= current_time:
-            self._birthdate = new_birthdate
-        else:
-            self._birthdate = current_time.strftime(format)
-
-    @property
-    def email(self) -> str:
-        return self._email
-    
-    @email.setter
-    def email(self, new_email:str) -> None:
-        valid_email = re.search("@[a-zA-Z0-9.-]+[.]com|org|edu|cc", new_email)
-        if valid_email:
-            self._email = new_email
-    
-    @property
-    def phone(self) -> str:
-        return self._phone
-    
-    @phone.setter
-    def phone(self, new_phone:str) -> None:
-        valid_phone = re.search("[2-9][0-9]{2}-[0-9]{3}-[0-9]{4}", new_phone)
-        if valid_phone:
-            self._phone = new_phone
