06d666c73594b955197139e4ab0d83d1a2006b36
Rachel <rad044@latech.edu>
2024-05-01 19:22:08 -0500

    added requests

--- gui/gui.py
+++ gui/gui.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home_page():
-    return render_template('test.html', header='Home page')
+    if request.method == "POST":
+        return render_template('test.html', header=(request.form.get("weight")))
+    else:
+        return render_template('test.html', header='Home page')
 
 @app.route('/alarms')
 def alarm_page():
