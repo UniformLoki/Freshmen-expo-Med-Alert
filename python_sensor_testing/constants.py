cc7625693eef06fbbfbda16cafe8e7c4657cbf57
Rachel <rad044@latech.edu>
2024-05-05 14:47:22 -0500

    began transitioning to using hx711py library

--- python_sensor_testing/constants.py
+++ python_sensor_testing/constants.py
@@ -1,8 +1,8 @@
 # Name: Rachel Dahl
 # Date: April 19, 2024
 # Description: Constants used in other files
-# This code uses the HX711 library by gandalf15, found here:
-# https://github.com/gandalf15/HX711
+# This code uses the hx711py library by j-dohnalek, found here:
+# https://github.com/j-dohnalek/hx711py/tree/master
 
 
 
@@ -13,12 +13,14 @@ from time import sleep
 
 
 #pin numbers
-DT_PIN = 0
-SCK_PIN = 0
+DT_PIN = 23
+SCK_PIN = 24
 
 
 #calibration
-CALIBRATION_FACTOR = 28510 #calculated in calibration.py file
+#calculated in calibration.py file
+OFFSET = 8418085.6875
+RATIO = 261.5
 
 #range in which weight changes are recognized (g)
 TOLERANCE = 0.1
