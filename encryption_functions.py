c928ff756a31ce0803bf816828f4a0c308ee0c9a
Rachel <rad044@latech.edu>
2024-05-06 01:22:27 -0500

    further testing of encryption functions

--- encryption_functions.py
+++ encryption_functions.py
@@ -3,15 +3,14 @@
 from dotenv import load_dotenv
 import rsa
 import os
-import keys
 
 #get keys
-
 load_dotenv()
 
 ENCRYPTION_KEY = rsa.PublicKey(int(os.getenv('ENCRYPT0')), int(os.getenv('ENCRYPT1')))
 DECRYPTION_KEY = rsa.PrivateKey(int(os.getenv('DECRYPT0')), int(os.getenv('DECRYPT1')), int(os.getenv('DECRYPT2')), int(os.getenv('DECRYPT3')), int(os.getenv('DECRYPT4')))
 
+#functions
 def encrypt(data:str)->str:
     """encrypt a string using a globally defined public key"""
     return rsa.encrypt(data.encode(), ENCRYPTION_KEY)
@@ -20,10 +19,11 @@ def decrypt(data:str)->str:
     """decrypt a string using a private key"""
     return rsa.decrypt(data, DECRYPTION_KEY).decode()
 
-#test
-test_string = "uiribreiubgiuergui"
-print(test_string)
-test_string = encrypt(test_string)
-print(test_string)
-test_string = decrypt(test_string)
-print(test_string)
+# test
+# test_string = "uiribreiubgiuergui"
+# print(test_string)
+# test_string = encrypt(test_string)
+# print(test_string)
+# test_string = decrypt(test_string)
+# print(test_string)
+
--- generate_keys.py
+++ generate_keys.py
@@ -6,8 +6,8 @@ import os
 #generate keys
 public_key, private_key = rsa.newkeys(512)
 
-os.environ["ENCRYPTION_KEY"] = public_key
-os.environ["DECRYPTION_KEY"] = private_key
-
 print(f"public key = {public_key}")
 print(f"private key = {private_key}")
+
+print("The keys are a set of comma separated values")
+print("copy/paste them in the .env file as ENCRYPT0, ENCRYPT1, DECRYPT0, etc")
