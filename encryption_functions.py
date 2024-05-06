ffda9064f84429482b121dc187cceb04b1f1a179
Rachel <rad044@latech.edu>
2024-05-06 00:47:26 -0500

    working on storing keys in env file

--- encryption_functions.py
+++ encryption_functions.py
@@ -1,23 +1,28 @@
 #encryption function wrappers for ease of use
 
+from dotenv import load_dotenv
 import rsa
+import os
+import keys
 
-#generate keys
-PUBLIC_KEY, PRIVATE_KEY = rsa.newkeys(512)
+#get keys
+load_dotenv()
+
+ENCRYPTION_KEY = keys.ENCRYPTION_KEY
+DECRYPTION_KEY = keys.DECRYPTION_KEY
 
 def encrypt(data:str)->str:
     """encrypt a string using a globally defined public key"""
-    return rsa.encrypt(data.encode(), PUBLIC_KEY)
+    return rsa.encrypt(data.encode(), ENCRYPTION_KEY)
 
 def decrypt(data:str)->str:
     """decrypt a string using a private key"""
-    return rsa.decrypt(data, PRIVATE_KEY).decode()
-
+    return rsa.decrypt(data, DECRYPTION_KEY).decode()
 
 #test
-# test_string = "uiribreiubgiuergui"
-# print(test_string)
-# test_string = encrypt(test_string)
-# print(test_string)
-# test_string = decrypt(test_string)
-# print(test_string)
+test_string = "uiribreiubgiuergui"
+print(test_string)
+test_string = encrypt(test_string)
+print(test_string)
+test_string = decrypt(test_string)
+print(test_string)
--- generate_keys.py
+++ generate_keys.py
@@ -1,7 +1,13 @@
+#generates ney encryption/decryption keys to be stored in env file
+
 import rsa
+import os
 
 #generate keys
 public_key, private_key = rsa.newkeys(512)
+
+os.environ["ENCRYPTION_KEY"] = public_key
+os.environ["DECRYPTION_KEY"] = private_key
 
 print(f"public key = {public_key}")
 print(f"private key = {private_key}")
