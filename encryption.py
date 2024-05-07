1e2bb0fef5c1ab3c50872e89c28814a8dd2eb35b
Rachel <rad044@latech.edu>
2024-05-07 11:05:29 -0500

    updated encryption functions, keys are now automatically stored in .env

--- encryption.py
+++ encryption.py
@@ -1,7 +1,13 @@
 import rsa
+import os
+from dotenv import load_dotenv
 
-#generate keys
-PUBLIC_KEY, PRIVATE_KEY = rsa.newkeys(512)
+load_dotenv()
+
+#get keys from .env file
+PUBLIC_KEY = rsa.PublicKey(int(os.getenv('ENCRYPT0')), int(os.getenv('ENCRYPT1')))
+PRIVATE_KEY = rsa.PrivateKey(int(os.getenv('DECRYPT0')), int(os.getenv('DECRYPT1')), int(os.getenv('DECRYPT2')), int(os.getenv('DECRYPT3')), int(os.getenv('DECRYPT4')))
+
 
 def encrypt(data:str)->str:
     """encrypt a string using a globally defined public key"""
